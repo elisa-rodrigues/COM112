25969d41534fa087dc712290993380a16259fcb6
Paulo Antonio Cerávolo <ceravolo.rodrigues@gmail.com>
2020-05-07 18:14:20 -0300

    Adicionando Ex3

--- Lista3/ex3.c
+++ Lista3/ex3.c
@@ -13,24 +13,30 @@
 
 // Vector Size
 #define vectorSize 10
+#define stringSize 50
 
 // Estrutura para os dados
-typedef struct funcionario{
-    char nome [50];
-    float salario;        
-}DadosFuncionario;
+typedef struct produto{
+    char codigo [stringSize];
+    char descricao [stringSize];
+    float preco;        
+}DadosProduto;
 
 
 // Protótipos de função
-int readVector(DadosFuncionario vet[]);
-int printVector(DadosFuncionario vet[]);
-int dummyData(DadosFuncionario vector[]);
-void insertionSort(DadosFuncionario arr[]);
+int readVector(DadosProduto vet[]);
+int printVector(DadosProduto vet[]);
+int dummyData(DadosProduto vector[]);
+void insertionSort(DadosProduto arr[]);
+int linearSearch(DadosProduto v[], int n, char value[]);
 
 int main()
 {
     // Definindo o Vetor
-    DadosFuncionario vector[vectorSize];  // Código para leitura de vetor  
+    DadosProduto vector[vectorSize];  // Código para leitura de vetor  
+    char searchValue[stringSize];
+    int compare;
+
 
     printf("\nPrograma de Ordenacao com SelectionSort\n\n");
 
@@ -51,85 +57,125 @@ int main()
     printVector(vector);
 
     printf("\n");
+
+    // Leitura do valor a ser pesquisado
+    printf("\nDigite um valor para pesquisar: ");
+    scanf("%s", &searchValue);
+    compare = linearSearch(vector, vectorSize, searchValue);
+
+    printf("\n\n --Número de comparações = %d", compare);
     return 0;
 }
 
 // Dummy Data
-int dummyData(DadosFuncionario vector[]){
+int dummyData(DadosProduto vector[]){
     
-    strcpy(vector[0].nome,"R. J. Dio");
-    vector[0].salario = 2000;
+    strcpy(vector[0].descricao,"Microdone do R. J. Dio");
+    strcpy(vector[0].codigo,"C001");
+    vector[0].preco = 2000;
 
-    strcpy(vector[1].nome,"Jeff Hanneman");
-    vector[1].salario = 20000;
+    strcpy(vector[1].descricao,"Paleta do Jeff Hanneman");
+    strcpy(vector[1].codigo,"C003");
+    vector[1].preco = 20000;
 
-    strcpy(vector[2].nome,"Dimebag Darrel");
-    vector[2].salario = 1000;
+    strcpy(vector[2].descricao,"Regata do Dimebag Darrel");
+    strcpy(vector[2].codigo,"C021");
+    vector[2].preco = 1000;
 
-    strcpy(vector[3].nome,"Tom Araya");
-    vector[3].salario = 592;
+    strcpy(vector[3].descricao,"Contrabaixo do Tom Araya");
+    strcpy(vector[3].codigo,"F012");
+    vector[3].preco = 592;
     
-    strcpy(vector[4].nome,"Phil Anselmo");
-    vector[4].salario = 3698;
+    strcpy(vector[4].descricao,"Bermuda do Phil Anselmo");
+    strcpy(vector[4].codigo,"F1233");
+    vector[4].preco = 3698;
     
-    strcpy(vector[5].nome,"Ritchie Blackmore");
-    vector[5].salario = 68423;
+    strcpy(vector[5].descricao,"Cachecol do Ritchie Blackmore");
+    strcpy(vector[5].codigo,"C11");
+    vector[5].preco = 68423;
 
-    strcpy(vector[6].nome,"Chuck Schuldiner");
-    vector[6].salario = 1255;
+    strcpy(vector[6].descricao,"Lembranca do Chuck Schuldiner");
+    strcpy(vector[6].codigo,"LCC19");
+    vector[6].preco = 1255;
 
-    strcpy(vector[7].nome,"Gary Holt");
-    vector[7].salario = 5264;
+    strcpy(vector[7].descricao,"Camiseta do Gary Holt");
+    strcpy(vector[7].codigo,"YCC019");
+    vector[7].preco = 5264;
 
-    strcpy(vector[8].nome,"Kerry King ");
-    vector[8].salario = 78944;
+    strcpy(vector[8].descricao,"Corrente do Kerry King ");
+    strcpy(vector[8].codigo,"DCC19");
+    vector[8].preco = 78944;
 
-    strcpy(vector[8].nome,"Nina Hagen");
-    vector[8].salario = 36885;
+    strcpy(vector[9].descricao,"Oculos da Nina Hagen");
+    strcpy(vector[9].codigo,"LC4C19");
+    vector[9].preco = 36885;
 
-    strcpy(vector[9].nome,"Dave Lombardo");
-    vector[9].salario = 5596;   
+
 }
 
 // Função para leitura do Vetor
 // Recebe um vetor como parâmetro
-int readVector(DadosFuncionario vet[])
+int readVector(DadosProduto vet[])
 {
     printf("Leitura do vetor:\n");
     for (int i = 0; i < vectorSize; i++)
     {
-        printf("Nome do funcionario[%d] = ", i + 1);
-        scanf("%s", &vet[i].nome);
-        printf("Salario do Funcionario[%d] = ", i + 1);
-        scanf("%f", &vet[i].salario);
+        printf("Codigo do produto[%d] = ", i + 1);
+        scanf("%s", &vet[i].codigo);
+        printf("Descricao do produto[%d] = ", i + 1);
+        scanf("%f", &vet[i].descricao);
+        printf("Preco do produto[%d] = ", i + 1);
+        scanf("%f", &vet[i].preco);
     }
     return 0;
 }
 
 // Função para impressão do Vetor
 // Recebe um vetor como parâmetro
-int printVector(DadosFuncionario vet[])
+int printVector(DadosProduto vet[])
 {
     for (int i = 0; i < vectorSize; i++)
-        printf("Funcionario[%d]\n - Nome: %s\n - Salario: %2.f\n", (i + 1), vet[i].nome, vet[i].salario);
+        printf("Produto[%d]\n - Codigo %s\n - Descricao: %s\n - Preco: %2.f\n", (i + 1), vet[i].codigo, vet[i].descricao, vet[i].preco);
     return 0;
 }
 
 // Função Insertion Sort
 // Recebe um vetor como parâmetro
-void insertionSort(DadosFuncionario arr[]) 
+void insertionSort(DadosProduto arr[]) 
 { 
-    int j; 
-    DadosFuncionario varAux, key;
+    int j, count; 
+    DadosProduto varAux, key;
     for (int i = 1; i < vectorSize; i++) { 
         key = arr[i]; 
         j = i - 1;           
-        while (j >= 0 && arr[j].salario > key.salario) { 
+        while (j >= 0 && arr[j].preco > key.preco) { 
             arr[j + 1] = arr[j]; 
             j = j - 1; 
         } 
         arr[j + 1] = key; 
     } 
 } 
+
+// Função de Busca Sequencial
+// Recebe um vetor e um valor de pesquisa como parâmetros
+int linearSearch(DadosProduto v[], int n, char value[])
+{
+    int i;
+    
+    for (i = 0; i < n; i++)
+    {
+        if (strcmp(v[i].codigo, value) == 0)
+        {
+            // Quando a chave é encontrada, houve (i+1) comparações (posição da chave no vetor acrescida de um)
+            // No MELHOR CASO a chave estará na primeira posição do vetor (1 comparação) => T(n) = 1
+            return i+1;  
+        }
+    }
+    
+    // Quando a chave NÃO é encontrada, houve i comparações (total de elementos do vetor)
+    // No PIOR CASO a chave não existe ou é o último elemento do vetor (n comparações) => T(n) = n
+    return i; 
+}
+
 
 
